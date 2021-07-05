60e1144f675dacb91590ee7b9e00ea301bc797e1
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-07-05 15:26:46 -0300

    Carousel

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+})
